240c5f9cd19b9931892dedd069fd1a0d7be61195
Ai Shi <shiai8901@gmail.com>
2017-07-19 16:31:42 -0700

    removed un-related files

--- server/server.js
+++ server/server.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const path = require('path');
-
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const port = 3000;
+const router = express.Router();
 
-app.use('/build', express.static(path.join(__dirname, '../build')));
-app.use('/static', express.static(path.join(__dirname, './static')));
+// Unsafely enable cors
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './static/index.html'));
+// logging middleware
+router.use(function(req, res, next) {
+    console.log('\nReceived:',{url: req.originalUrl, body: req.body, query: req.query})
+    next()
+})
+// router.use(express.static(path.join(__dirname, '../public')));
+// app.use('/static', express.static(path.join(__dirname, './static')));
+
+router.get('/', (req, res) => {
+  res.end('hello');
 });
 
-app.listen(port, () => {
-  console.log(`Ulsan Webapp Server lisiening at ${port}`);
-});
+app.use('/api', router);
+app.listen(port);
+console.log(`API running at localhost:${port}/api`);
