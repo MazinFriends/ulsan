fdb33a060a887efe55fae7d61d6d226191a5af83
Ai Shi <shiai8901@gmail.com>
2017-07-19 21:41:22 -0700

    complete react component

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import styles from '../../dist/panel.less';
+
+import SignUp from './SignUp';
+import LogIn from './LogIn';
 
 const ForgotPassword = () => (
-		<div> 
-		<h3>Forgot Password</h3>
+	<div className="center-container">
+		<div className="panel">
+			<div className="panel-heading">
+				<h2>Sign Up</h2>
+			</div>
+			<div className="panel-body">
+				<form>
+					<label>We will send you an email to reset password</label>
+					<input type="text" placeholder="Email" name="email" required />
+					<input type="submit" className= "panel-button" value="Send" />
+				</form>
+			</div>
 		</div>
-	)
+		<div className="panel-footer">
+			<div className="links">
+				<div className="link"><Link to="/">Log In</Link></div>
+				<div><hr></hr></div>
+				<div className="link"><Link to="/signup">Create an account</Link></div>		
+			</div>
+			<p>By logging in, I certify that I agree with the Terms of Service EULA and Privacy Policy.</p>
+		</div>
+	</div>
+)
 
 
 export default ForgotPassword;
